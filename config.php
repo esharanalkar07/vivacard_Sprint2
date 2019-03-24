947503696db0cb5b963b1f54752de77f166902b7
esharanalkar07 <47105661+esharanalkar07@users.noreply.github.com>
2019-03-24 16:27:09 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,23 +1,3 @@
-<?/*php
-session_start();
-
-// Define database
-define('dbhost', 'vivacard.database.windows.net');
-define('dbuser', 'vivacard');
-define('dbpass', 'Mysql123');
-define('dbname', 'db_group_e_cm004');
-
-
-// Connecting database
-try {
-    $connect = new PDO("mysql:host=".dbhost."; dbname=".dbname, dbuser, dbpass);
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e) {
-    echo $e->getMessage();
-}
-*/
-?>
 
 <? php
 $connectstr_dbhost = 'vivacard.database.windows.net';
