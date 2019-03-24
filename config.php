fd1a5448242e4f19ce88e8eeecaafc79871597dd
esharanalkar07 <47105661+esharanalkar07@users.noreply.github.com>
2019-03-24 17:36:16 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,10 @@
 <?php
-$host = 'vivacard.database.windows.net';
-$username = 'vivacard';
-$password = 'Mysql123';
-$db_name = 'vivacarddb';
-
-//Establishes the connection
-$connect = mysqli_init();
-mysqli_real_connect($connect, $host, $username, $password, $db_name, 3306);
-if (mysqli_connect_errno($conn)) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
+    $serverName = "vivacard.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "vivacarddb", // update me
+        "Uid" => "vivacard@vivacard", // update me
+        "PWD" => "Mysql123" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+?>
