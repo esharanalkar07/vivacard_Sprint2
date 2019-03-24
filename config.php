c9cea10dfc89ca7936caf38efb4d98390ad45042
esharanalkar07 <47105661+esharanalkar07@users.noreply.github.com>
2019-03-24 17:11:48 +0000

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,9 @@
     );
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
+
+
+   /* $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
          FROM [SalesLT].[ProductCategory] pc
          JOIN [SalesLT].[Product] p
          ON pc.productcategoryid = p.productcategoryid";
@@ -19,4 +21,5 @@
      echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
     }
     sqlsrv_free_stmt($getResults);
+    */
 ?>
