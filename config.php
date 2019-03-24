38214d0eaaee0d6c4281aebadd2f3b9e031c87d3
esharanalkar07 <47105661+esharanalkar07@users.noreply.github.com>
2019-03-24 20:14:17 +0000

    Update config.php

--- config.php
+++ config.php
@@ -17,4 +17,5 @@ if (!$link) {
  echo "Error: Unable to connect to MySQL." . PHP_EOL;
  echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
  echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+ exit;
  ?>
