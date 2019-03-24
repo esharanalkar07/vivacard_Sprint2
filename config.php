9464878acb298471b8c42c8cc60b805fef3976c6
esharanalkar07 <47105661+esharanalkar07@users.noreply.github.com>
2019-03-24 16:25:29 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
-<?php
+<?/*php
 session_start();
 
 // Define database
@@ -16,5 +16,29 @@ try {
 catch(PDOException $e) {
     echo $e->getMessage();
 }
-
+*/
 ?>
+
+<? php
+$connectstr_dbhost = 'vivacard.database.windows.net';
+$connectstr_dbname = 'db_group_e_cm004';
+$connectstr_dbusername = 'vivacard';
+$connectstr_dbpassword = 'Mysql123';
+foreach ($_SERVER as $key => $value) {
+ if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+ continue;
+ }
+ $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
+if (!$link) {
+ echo "Error: Unable to connect to MySQL." . PHP_EOL;
+ echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+ echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+ exit;
+}
+?>
+
