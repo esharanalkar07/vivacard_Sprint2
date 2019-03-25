4e328fd1d04f7143aeafcd1abfce085cff0a9750
esharanalkar07 <47105661+esharanalkar07@users.noreply.github.com>
2019-03-25 15:34:22 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,24 @@
 <? php
+
+// Define database
+define('dbhost', 'vivacardshare.scm.azurewebsites.net');
+define('dbuser', '');
+define('dbpass', '');
+define('dbname', 'localdb');
+
+
+// Connecting database
+try {
+    $connect = new PDO("mysql:host=".dbhost."; dbname=".dbname, dbuser, dbpass);
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+    echo $e->getMessage();
+}
+
+
+
+?>
  
  
  
- 
- $connect = new PDO("sqlsrv:Server=vivacardshare.scm.azurewebsites.net;Database=localdb", "", "");
- 
- ?>
