e61744256c1a0cc373f5a71b92b25a3126c29a13
esharanalkar07 <47105661+esharanalkar07@users.noreply.github.com>
2019-03-25 13:07:26 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,26 +1,20 @@
 <?php
-$connectstr_dbhost = ' 127.0.0.1';
-$connectstr_dbname = 'localdb';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
-foreach ($_SERVER as $key => $value) {
- if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
- continue;
- }
- $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
- $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
- $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
- $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+session_start();
+
+// Define database
+define('dbhost', ' 127.0.0.1');
+define('dbuser', '');
+define('dbpass', '');
+define('dbname', 'localdb');
+
+
+// Connecting database
+try {
+    $connect = new PDO("mysql:host=".dbhost."; dbname=".dbname, dbuser, dbpass);
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-$connect = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
-if (!$connect) {
- echo "Error: Unable to connect to MySQL." . PHP_EOL;
- echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
- echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
- exit;
+catch(PDOException $e) {
+    echo $e->getMessage();
 }
-else 
-{
- echo "connection successful";
-}
+
 ?>
