8060531d8bcd308317c585eaaecfa3445e03a501
esharanalkar07 <47105661+esharanalkar07@users.noreply.github.com>
2019-03-25 13:10:08 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,20 +1,22 @@
-<?php
-session_start();
-
-// Define database
-define('dbhost', ' 127.0.0.1');
-define('dbuser', '');
-define('dbpass', '');
-define('dbname', 'localdb');
-
-
-// Connecting database
-try {
-    $connect = new PDO("mysql:host=".dbhost."; dbname=".dbname, dbuser, dbpass);
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+<?
+$connectstr_dbhost = 'vivacardshare.scm.azurewebsites.net';
+$connectstr_dbname = 'localdb';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+foreach ($_SERVER as $key => $value) {
+ if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+ continue;
+ }
+ $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
-catch(PDOException $e) {
-    echo $e->getMessage();
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
+if (!$link) {
+ echo "Error: Unable to connect to MySQL." . PHP_EOL;
+ echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+ echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+ exit;
 }
-
 ?>
