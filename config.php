bccff8b5d04eaf35217208352f0e2cadb4382b84
esharanalkar07 <47105661+esharanalkar07@users.noreply.github.com>
2019-03-25 15:17:12 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,26 +1,8 @@
-<?
-$connectstr_dbhost = 'vivacardshare.scm.azurewebsites.net';
-$connectstr_dbname = 'localdb';
-//$connectstr_dbusername = 'root';
-$connectstr_dbpassword = 'pwd';
-foreach ($_SERVER as $key => $value) {
- if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
- continue;
- }
- $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
- $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
- $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
- $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-$connect = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
-if (!$connect) {
- echo "Error: Unable to connect to MySQL." . PHP_EOL;
- echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
- echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
- exit;
-}
-else 
-{
- echo "connection successfull";
-}
-?>
+<? php
+ 
+ 
+ 
+ 
+ $connect = new PDO("sqlsrv:Server=vivacardshare.scm.azurewebsites.net;Database=localdb", "", "");
+ 
+ ?>
